23bd311222e41d0594506aa2a2c84cfff1c329b9
francosorbello <franco.sorbelo@gmail.com>
2022-11-17 12:58:24 -0300

    added basic example of editor input in vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import EditorText from './components/EditorText.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <EditorText responsive=false></EditorText>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
---
+++ src/components/EditorText.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+/**
+ * Input donde el usuario puede escribir texto
+ * 
+ * responsive: Define si el editor es responsive o no
+ * width: ancho del medido en rem. Solo se utiliza si el editor no es responsive
+ * height: medido en rem. Solo se utiliza si el editor no es responsive
+ */
+export default {
+    name: "EditorText",
+    props: {
+        responsive: Boolean,
+        width: Number,
+        height: Number
+    },
+    methods: {
+        onClick(){
+            console.log(document.getElementById("h1piola")?.innerText)
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1 contenteditable="true" id="h1piola">
+            {{responsive}}
+        </h1>
+    <button @click="onClick">
+        Click me
+    </button>
+    </div>
+</template>
